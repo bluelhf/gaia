bcfbf8ce02fd69786c7d65b6c7eef7e67aed6d0c
bluelhf <ilari.suhonen@gmail.com>
2024-03-17 21:42:05 +0200

    feat(lib): tokio support! 💝

--- gaia/src/lib.rs
+++ gaia/src/lib.rs
@@ -23,6 +23,9 @@ type StreamTagLength = <Cipher as AeadCore>::TagSize;
 #[cfg(feature = "base64")]
 pub mod keystore;
 
+#[cfg(feature = "tokio")]
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, AsyncReadExt};
+
 const BUF_SIZE: usize = 16384;
 
 pub fn encrypt(input: impl Read, mut output: impl Write) -> Result<(Key<Cipher>, stream::Nonce<Cipher, Stream>), GaiaError> {
@@ -71,6 +74,61 @@ where
             output.write_all(&*decryptor.decrypt_next(&buffer[..]).map_err(|e| GaiaError::Decrypting(e))?).map_err(|e| GaiaError::WritingOutput(e))?;
         } else {
             output.write_all(&*decryptor.decrypt_last(&buffer[..]).map_err(|e| GaiaError::Decrypting(e))?).map_err(|e| GaiaError::WritingOutput(e))?;
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(feature = "tokio")]
+pub async fn encrypt_async(input: impl tokio::io::AsyncRead + Unpin, mut output: impl tokio::io::AsyncWrite + Unpin) -> Result<(Key<Cipher>, stream::Nonce<Cipher, Stream>), GaiaError> {
+    let key = Cipher::generate_key(&mut OsRng);
+
+    let mut stream_nonce = GenericArray::<u8, StreamNonceLength>::default();
+    (&mut OsRng).fill_bytes(&mut stream_nonce);
+
+    let mut encryptor = Encryptor::<Cipher, Stream>::new(&key, &stream_nonce.into());
+
+    let mut reader = tokio::io::BufReader::new(input);
+    let mut buffer = Vec::new();
+
+    loop {
+        buffer.clear();
+        reader.get_mut().take(BUF_SIZE as u64).read_to_end(&mut buffer).await.map_err(|e| GaiaError::ReadingInput(e))?;
+
+        if let Ok(more_data) = reader.fill_buf().await && more_data != [] {
+            output.write_all(&*encryptor.encrypt_next(&buffer[..]).map_err(|e| GaiaError::Encrypting(e))?).await.map_err(|e| GaiaError::WritingOutput(e))?;
+        } else {
+            output.write_all(&*encryptor.encrypt_last(&buffer[..]).map_err(|e| GaiaError::Encrypting(e))?).await.map_err(|e| GaiaError::WritingOutput(e))?;
+            break;
+        }
+    }
+
+    Ok((key, stream_nonce))
+}
+
+#[cfg(feature = "tokio")]
+pub async fn decrypt_async(input: impl tokio::io::AsyncRead + Unpin, kh: &(Key<Cipher>, stream::Nonce<Cipher, Stream>), mut output: impl tokio::io::AsyncWrite + Unpin) -> Result<(), GaiaError>
+where
+    <Cipher as AeadCore>::NonceSize: Sub<U4>,
+    <<Cipher as AeadCore>::NonceSize as Sub<U4>>::Output: ArrayLength<u8>
+{
+    let stream_nonce = kh.1;
+
+    let mut decryptor = Decryptor::<Cipher, Stream>::new(&kh.0, &stream_nonce.into());
+    let mut reader = tokio::io::BufReader::new(input);
+
+    let mut buffer = Vec::new();
+
+    loop {
+        buffer.clear();
+        reader.get_mut().take(BUF_SIZE as u64 + StreamTagLength::to_u64()).read_to_end(&mut buffer).await.map_err(|e| GaiaError::ReadingInput(e))?;
+
+        if let Ok(more_data) = reader.fill_buf().await && more_data != [] {
+            output.write_all(&*decryptor.decrypt_next(&buffer[..]).map_err(|e| GaiaError::Decrypting(e))?).await.map_err(|e| GaiaError::WritingOutput(e))?;
+        } else {
+            output.write_all(&*decryptor.decrypt_last(&buffer[..]).map_err(|e| GaiaError::Decrypting(e))?).await.map_err(|e| GaiaError::WritingOutput(e))?;
             break;
         }
     }
