14a662f2b13575435967f205a96f6be3768bb34c
bluelhf <ilari.suhonen@gmail.com>
2024-03-22 15:00:29 +0200

    chore(cli): async :)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod error;
 
 use std::borrow::Cow;
-use std::fs::{File, OpenOptions};
 use std::io;
-use std::io::{Read, stderr, stdin, stdout, Write};
 use clap::{Parser, Subcommand};
 use main_error::MainError;
-use gaia::{decrypt, encrypt, keystore::{from_secret, to_secret}};
+use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
+use gaia::{decrypt_async, encrypt_async, keystore::{from_secret, to_secret}};
 use crate::error::CliError;
 
 #[derive(Parser, Debug)]
@@ -38,7 +37,8 @@ enum Commands {
     }
 }
 
-fn main() -> Result<(), MainError> {
+#[tokio::main]
+async fn main() -> Result<(), MainError> {
     let args = Args::parse();
 
     fn input_name(path: &str) -> String {
@@ -53,43 +53,43 @@ fn main() -> Result<(), MainError> {
         }
     }
 
-    fn open_input(path: &str) -> Result<Box<dyn Read>, io::Error> {
+    async fn open_input(path: &str) -> Result<Box<dyn AsyncRead + Unpin>, io::Error> {
         return Ok(if path == "-" {
-            Box::new(stdin())
+            Box::new(tokio::io::stdin())
         } else {
-            Box::new(File::open(path)?)
+            Box::new(tokio::fs::File::open(path).await?)
         })
     }
 
-    fn open_output(path: &str) -> Result<Box<dyn Write>, io::Error> {
+    async fn open_output(path: &str) -> Result<Box<dyn AsyncWrite + Unpin>, io::Error> {
         return Ok(match path {
-            "-" => Box::new(stdout()),
-            "^" => Box::new(stderr()),
-            _ => Box::new(OpenOptions::new()
-                .create(true).write(true).truncate(true).open(path)?)
+            "-" => Box::new(tokio::io::stdout()),
+            "^" => Box::new(tokio::io::stderr()),
+            _ => Box::new(tokio::fs::OpenOptions::new()
+                .create(true).write(true).truncate(true).open(path).await?)
         })
     }
 
     match &args.command {
         Commands::Encrypt { file_path: input, output, secret_path } => {
-            let input_file = open_input(input).map_err(|e| CliError::OpeningInput(input_name(input), e))?;
-            let output_file = open_output(output).map_err(|e| CliError::OpeningOutput(output_name(output), e))?;
-            let mut secret_file = open_output(secret_path).map_err(|e| CliError::WritingSecret(output_name(secret_path), e))?;
+            let input_file = open_input(input).await.map_err(|e| CliError::OpeningInput(input_name(input), e))?;
+            let output_file = open_output(output).await.map_err(|e| CliError::OpeningOutput(output_name(output), e))?;
+            let mut secret_file = open_output(secret_path).await.map_err(|e| CliError::WritingSecret(output_name(secret_path), e))?;
 
-            let handle = encrypt(input_file, output_file)?;
+            let handle = encrypt_async(input_file, output_file).await?;
 
             let secret = to_secret(&handle).map_err(|e| CliError::InvalidSecret(e))?;
-            secret_file.write_all(secret.as_bytes()).map_err(|e| CliError::WritingSecret(output_name(secret_path), e))?;
-            secret_file.write(&[10])?;
+            secret_file.write_all(secret.as_bytes()).await.map_err(|e| CliError::WritingSecret(output_name(secret_path), e))?;
+            secret_file.write(&[10]).await?;
 
             Ok(())
         }
         Commands::Decrypt { secret_key, file_path: input, output } => {
-            let input_file = open_input(input).map_err(|e| CliError::OpeningInput(input_name(input), e))?;
-            let output_file = open_output(output).map_err(|e| CliError::OpeningOutput(output_name(output), e))?;
+            let input_file = open_input(input).await.map_err(|e| CliError::OpeningInput(input_name(input), e))?;
+            let output_file = open_output(output).await.map_err(|e| CliError::OpeningOutput(output_name(output), e))?;
 
-            Ok(decrypt(input_file, &from_secret(secret_key.as_ref())
-                .map_err(|e| CliError::InvalidSecret(e))?, output_file)?)
+            Ok(decrypt_async(input_file, &from_secret(secret_key.as_ref())
+                .map_err(|e| CliError::InvalidSecret(e))?, output_file).await?)
         }
     }
 }
